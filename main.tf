8ba79b0225a51e205515b1e058589111cdd53a73
Jay Stockhausen <jay.stockhausen@gmail.com>
2023-08-13 23:51:49 +0000

    add ec2

--- main.tf
+++ main.tf
@@ -12,11 +12,36 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_vpc" "example" {
+resource "aws_vpc" "private" {
   cidr_block = "10.0.0.0/16"
   
   tags = {
     Name = "tfcloud"
     sentinel = ""
+  }
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "general" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.large"
+
+  tags = {
+    Name = "general"
   }
 }
