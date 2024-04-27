b7ff524d185a2cbbb8b03c7361b9a4a97dbf2418
Jay Stockhausen <jay.stockhausen@gmail.com>
2024-04-27 09:07:39 -0600

    ubuntu 24 instance

--- main.tf
+++ main.tf
@@ -60,3 +60,35 @@ resource "aws_instance" "general" {
     schedule = "stop-at-10"
   }
 }
+
+data "aws_ami" "ubuntu_24" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "ubuntu_24" {
+  ami           = data.aws_ami.ubuntu_24.id
+  instance_type = "t2.large"
+  key_name      = "macos"
+
+  root_block_device {
+    volume_size = 20
+  }
+
+  tags = {
+    Name     = "ubuntu-24"
+    test     = "tfcloud"
+    schedule = "stop-at-10"
+  }
+}
